f90d9f6fba8917ed1fa7cbd04deea70574523f30
Elena Kichinskaia <nitress@gmail.com>
2020-11-05 17:31:47 +0200

    Adaptive site helfway

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+(() => {
+    const menuBtnRef = document.querySelector('[data-menu-button]');
+
+    menuBtnRef.addEventListener('click', () => {
+        
+        menuBtnRef.classList.toggle('is-open');
+    });
+})();
