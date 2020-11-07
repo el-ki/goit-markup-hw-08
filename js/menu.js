43e6811b872d1d7fb9f4e793d904076819be080d
Elena Kichinskaia <nitress@gmail.com>
2020-11-07 19:21:14 +0200

    js opens and closes menu on the mobile version

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 (() => {
-    const menuBtnRef = document.querySelector('[data-menu-button]');
-
-    menuBtnRef.addEventListener('click', () => {
-        
-        menuBtnRef.classList.toggle('is-open');
-    });
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  menuBtnRef.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
